e1b6bb405f916fa8c0fd0d388f8fd089af676323
GMcLachlan45 <gmclachlan45@gmail.com>
2023-03-15 08:18:00 -0700

    Add CPP solution to valid palindrome

---
+++ easy/0125-valid-palindrome/cpp/GM-rev-01.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        short int end = s.length() - 1;
+        short int start = 0;
+
+        while(start < end) {
+            if (!std::isalnum(s[start])) {
+                start++;
+            } else if (!std::isalnum(s[end])) {
+                end--;
+            } else {
+                if(s[start]!=s[end] && (std::isdigit(s[start]) || std::isdigit(s[end]))){
+                    std::cout << "here22";
+                    return false;
+                } else if (s[start] == s[end] || s[start] == s[end] + 32 || s[start] == s[end] - 32) {
+                    start++;
+                    end--;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
