6ec63723d2a32ceedfaa88e2b354db0dbeb7919c
OneInA1E6 <andrew.dunn213@gmail.com>
2023-03-19 14:20:02 -0700

    Sum leetcode bank

---
+++ easy/1716-calculate-money-in-leetcode-bank/c#/AD_1.cs
@@ -0,0 +1,9 @@
+public class Solution {
+    public int TotalMoney(int n) {
+        int numWeeks = n/7;
+        int numDays = n%7;
+        int sum = (4*numWeeks)*7 + 7*(((numWeeks-1)*numWeeks)/2);
+        sum += ((numDays+numWeeks)*(numDays+numWeeks+1)/2) - ((numWeeks)*(numWeeks+1)/2);
+        return sum;
+    }
+}
