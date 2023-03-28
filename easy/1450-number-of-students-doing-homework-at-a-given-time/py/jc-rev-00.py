d4a775cc55dbbac3e9be56ab6d40b36f772faf06
jcoll059 <jordancolledge059@gmail.com>
2023-03-28 07:27:32 -0700

    Finished 1450 in Python

---
+++ easy/1450-number-of-students-doing-homework-at-a-given-time/py/jc-rev-00.py
@@ -0,0 +1,15 @@
+# runtime: 15ms
+# memory: 13.3MB
+class Solution(object):
+    def busyStudent(self, startTime, endTime, queryTime):
+        """
+        :type startTime: List[int]
+        :type endTime: List[int]
+        :type queryTime: int
+        :rtype: int
+        """
+        count = 0
+        for i in range(len(startTime)):
+            if startTime[i] <= queryTime and endTime[i] >= queryTime:
+                count += 1
+        return count
