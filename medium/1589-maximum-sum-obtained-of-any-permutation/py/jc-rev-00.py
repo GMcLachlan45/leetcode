494dfff214b17ee9365e854322c8f712c5b01c7c
jcoll059 <jordancolledge059@gmail.com>
2023-04-03 14:07:54 -0700

    improved 1589, better way of getting counts

--- medium/1589-maximum-sum-obtained-of-any-permutation/py/jc-rev-00.py
+++ medium/1589-maximum-sum-obtained-of-any-permutation/py/jc-rev-00.py
@@ -6,12 +6,17 @@ class Solution(object):
         :type requests: List[List[int]]
         :rtype: int
         """
-
+        n = len(nums)
         st = sorted(nums)
         counts = np.zeros(len(nums), dtype=int)
         for rq in requests:
-            for i in range(rq[0], rq[1]+1):
-                counts[i] = counts[i] + 1
+            counts[rq[0]] += 1
+            if rq[1] < n-1:
+                counts[rq[1]+1] -= 1
+        for i in range(n):
+            if i == 0:
+                continue
+            counts[i] += counts[i-1]
         
         indices = np.argsort(counts)
 
