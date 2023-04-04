a8ab4b8d2b0147b562854c17632363c6eef906c5
GMcLachlan45 <gmclachlan45@gmail.com>
2023-04-04 07:12:03 -0700

    Revert "2 lines solution"
    This reverts commit e193bb44986b1cf12cab71113e272f305b7c82c0.

--- easy/0575-distribute-candies/cpp/gm-rev-00.cpp
+++
@@ -1,9 +0,0 @@
-// Runtime 265ms
-// Memory 110.4MB
-class Solution {
-public:
-    int distributeCandies(vector<int>& candyType) {
-        std::unordered_set types(candyType.begin(), candyType.end());
-        return std::min(candyType.size()/2, types.size());
-    }
-};
