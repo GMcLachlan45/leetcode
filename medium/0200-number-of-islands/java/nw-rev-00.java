fb5317f624c081d2350edcd534d74cd924860c6f
Nathan Wright <44069341+DapperShark1@users.noreply.github.com>
2023-04-06 15:32:13 -0700

    Create nw-rev-00.java

---
+++ medium/0200-number-of-islands/java/nw-rev-00.java
@@ -0,0 +1,30 @@
+//3ms
+//50.9MB
+class Solution {
+    private void dfs(char[][] grid, int i, int j){
+        if(i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] != '1')
+            return;
+        grid[i][j] = '2'; 
+        dfs(grid, i+1, j);
+        dfs(grid, i, j+1);
+        dfs(grid, i-1, j);
+        dfs(grid, i, j-1);
+    }
+
+    public int numIslands(char[][] grid) {
+        int islands = 0;
+        int m = grid.length;
+        if(m == 0)
+            return 0;
+        int n = grid[0].length;
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(grid[i][j] == '1'){
+                    dfs(grid, i, j);
+                    islands++;
+                }
+            }
+        }
+        return islands;
+    }
+}
