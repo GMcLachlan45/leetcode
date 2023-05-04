28e76896fc162a20aa1a5da7f41e59fe25a24df0
GMcLachlan45 <gmclachlan45@gmail.com>
2023-05-04 16:05:09 -0700

    Add slick 15 minute ans

---
+++ medium/2182-construct-string-with-repeat-limit/cpp/gm-rev-00.cpp
@@ -0,0 +1,26 @@
+// Runtime 150ms
+// Memory 20.1mb
+class Solution {
+public:
+    string repeatLimitedString(string s, int repeatLimit) {
+        std::sort(s.begin(), s.end(), std::greater<char>());
+
+        int next_char = 0;
+        int char_count = 1;
+        for(int i = 1; i < s.size(); i++) {
+            if(char_count >= repeatLimit && s[i-1] == s[i]) {
+                next_char = std::max(next_char, i);
+                while(next_char < s.size() && s[i] == s[next_char])
+                    next_char++;
+                if(next_char >= s.size())
+                    return s.substr(0, i);
+
+                s[i] = s[next_char];
+                s[next_char] = s[i-1];
+                next_char++;
+            } else
+                char_count = (s[i-1] != s[i]) ? 1 : char_count + 1;
+        }
+        return s;
+    }
+};
