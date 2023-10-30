ac248becb538967a0453edb5f39ef52d7bf0d113
gmclachlan45 <gmclachlan45@gmail.com>
2023-10-30 07:42:45 -0700

    Make a bad time one?

---
+++ leetcode/easy/1356-sort-integers-by-the-number-of-1-bits/cpp/gm-rev-00.cpp
@@ -0,0 +1,13 @@
+// Runtime: 10ms
+// Memory: 10.4MB
+class Solution {
+public:
+    vector<int> sortByBits(vector<int>& arr) {
+        std::sort(arr.begin(), arr.end(), [](int a, int b) {
+            if(std::bitset<14>(a).count() == std::bitset<14>(b).count())
+                return a < b;
+            return std::bitset<14>(a).count() < std::bitset<14>(b).count();
+        });
+        return arr;
+    }
+};
