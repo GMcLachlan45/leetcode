4c085d42e0ba7781d36999a52d954008556c55e8
gmclachlan45 <gmclachlan45@gmail.com>
2023-11-06 08:54:19 -0800

    Oof. O(n) memory and O(n) worst runtime does that to a dude.

---
+++ leetcode/medium/1845-seat-reservation-manager/cpp/gm-rev-00.cpp
@@ -0,0 +1,28 @@
+class SeatManager {
+    std::stack<int> seats;
+public:
+    SeatManager(int n) {
+        while(n>0) {
+            seats.push(n--);
+        }
+    }
+
+    int reserve() {
+        int ret = seats.top();
+        seats.pop();
+        return ret;
+    }
+
+    void unreserve(int seatNumber) {
+        std::stack<int> lowerSeats;
+        while(!seats.empty() && seatNumber > seats.top()) {
+            lowerSeats.push(seats.top());
+            seats.pop();
+        }
+        seats.push(seatNumber);
+        while(!lowerSeats.empty()) {
+            seats.push(lowerSeats.top());
+            lowerSeats.pop();
+        }
+    }
+};
