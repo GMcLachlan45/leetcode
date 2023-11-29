b51cdd7b174aec78257df927864dc7f58585ed56
gmclachlan45 <gmclachlan45@gmail.com>
2023-11-29 08:41:24 -0800

    Cool, actually make it work

--- advent-of-code/2015/day1/cpp/gm-rev-00.cpp
+++ advent-of-code/2015/day1/cpp/gm-rev-00.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <fstream>
 int main() {
-    std::cout<< "Hello World"<< cout;
+    std::cout<< "Hello World"<< std::endl;
+    ofstream myfile("./input.txt");
+    return 0;
 }
