82d6b2bfa4e50c352aaabc5342045a55fb36735d
gmclachlan45 <gmclachlan45@gmail.com>
2023-11-29 09:12:45 -0800

    Solve AOC 2015 day 1

--- advent-of-code/2015/day1/cpp/gm-rev-00.cpp
+++ advent-of-code/2015/day1/cpp/gm-rev-00.cpp
@@ -1,7 +1,23 @@
-#include <iostream>
-#include <fstream>
+#include <string>
+// A helper function to easily read the player's input
+#include "../../../bin/input-reader.cpp"
 int main() {
-    std::cout<< "Hello World"<< std::endl;
-    ofstream myfile("./input.txt");
+    std::cout<< "Hello AoC 2015!"<< std::endl;
+    std::string input= readInput();
+    int floor = 0;
+    int firstBasement = -1;
+    for(int i = 0; i < input.size(); i++) {
+        if(input[i]=='(')
+            floor++;
+        else {
+            floor--;
+            if(firstBasement == -1 && floor < 0)
+                firstBasement = i+1;
+        }
+    }
+    // Passed
+    std::cout<< "Solution to part 1: Santa's final floor is floor "<< floor << std::endl;
+    // Passed
+    std::cout<< "Solution to part 2: Santa is in the basement after "<< firstBasement << " steps" << std::endl;
     return 0;
 }
