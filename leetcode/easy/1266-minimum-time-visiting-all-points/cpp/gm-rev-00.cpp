9b4539281966b034ac2ee4fedd2efc7cd42aa070
gmclachlan45 <gmclachlan45@gmail.com>
2023-12-03 09:55:24 -0800

    3 easys in a row, let's goo

---
+++ leetcode/easy/1266-minimum-time-visiting-all-points/cpp/gm-rev-00.cpp
@@ -0,0 +1,11 @@
+// Runtime: 3ms
+// Memory: 10.44MB
+class Solution {
+public:
+    int minTimeToVisitAllPoints(vector<vector<int>>& points) {
+        int time = 0;
+        for(int i = 1; i < points.size(); i++)
+            time+= std::max(std::abs(points[i][0] - points[i-1][0]), std::abs(points[i][1] - points[i-1][1]));
+    return time;
+    }
+};
