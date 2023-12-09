bede4ede66a61deb4be68b0254c9f9f8e78b6779
gmclachlan45 <gmclachlan45@gmail.com>
2023-12-08 22:44:22 -0800

    Washed

--- advent-of-code/2023/day9/cpp/gm-rev-00.cpp
+++ advent-of-code/2023/day9/cpp/gm-rev-00.cpp
@@ -43,19 +43,16 @@ int main(int argc, char* argv[]) {
             for(int i = 1; i < info[info.size()-2].size(); i++) {
                 info[info.size()-1].push_back(info[info.size()-2][i]-info[info.size()-2][i-1]);
                 allZero = allZero && info[info.size()-1][i-1] == 0;
-
             }
         }
 
         info[info.size()-1].push_back(0);
-        for(int b = info.size()-1; b > 0; b--) {
+        for(int b = info.size()-1; b > 0; b--)
             info[b-1].push_back(info[b][info[b].size()-1] + info[b-1][info[b-1].size()-1]);
-        }
         info[info.size()-1].push_back(0);
         sum+= info[0][info[0].size()-1];
-        for(int b = info.size()-1; b > 0; b--) {
+        for(int b = info.size()-1; b > 0; b--)
             info[b-1].push_back(-info[b][info[b].size()-1] + info[b-1][0]);
-        }
         sum2+= info[0][info[0].size()-1];
 
     }
@@ -64,7 +61,6 @@ int main(int argc, char* argv[]) {
     std::cout<< "Solution 1: the sum of the extrapolated values going forward is " << sum <<std::endl;
     // Passed
     std::cout<< "Solution 2: the sum of the extrapolated values going backward is " << sum2 <<std::endl;
-
     return 0;
 
 }
