2d63d462e103313410c1c3f856b194077bd2f300
emp <emp@worktop>
2024-10-07 08:42:17 -0700

    Seeing as how I'm the only one going through coding now...

---
+++ book-questions/accelerated-cpp/ch-00/04.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+int main() {
+    std::cout << "// a small C++ program" << std::endl
+              << "#include <iostream>" << std::endl
+              << std::endl
+              << "int main()" << std::endl
+              << "\tstd::cout << \"Hello, world!\" << std::endl;" << std::endl
+              << "\treturn 0;" << std::endl
+              << "}" << std::endl;
+}
